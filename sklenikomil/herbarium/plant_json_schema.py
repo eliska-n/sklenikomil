0fd816b7aa1deee9bcfa048a70b58dcaf4186aaa
enovotna <eliska.novotna@teskalabs.com>
2025-01-12 15:31:15 +0100

    adjust data structure so it fits the UI

--- sklenikomil/herbarium/plant_json_schema.py
+++ sklenikomil/herbarium/plant_json_schema.py
@@ -5,7 +5,7 @@ plant_json_schema = {
 		"latin": {"type": "string"},
 		"seed_to_harvest_days": {"type": "integer"},
 		"pre_grow_days": {"type": "integer"},
-		"seed_ranges": {"type": "array", "items": {"type": "array", "items": {"type": "integer"}}},
+		"seed_ranges": {"type": "array", "items": {"type": "object", "properties": {"max": {"type": "integer"}, "min": {"type": "integer"}}}},
 		"categories": {"type": "array", "items": {"type": "string"}},
 		"icon": {"type": "string"},
 		"literature": {"type": "array", "items": {"type": "string"}},
